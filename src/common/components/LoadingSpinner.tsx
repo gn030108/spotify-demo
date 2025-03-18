e8e0d65ee58d556e2a2ffbb112ee254290fa7656
Wonwoo Park <gn030108@gmail.com>
2025-03-18 23:02:46 +0900

    스피너 색상변경

--- src/common/components/LoadingSpinner.tsx
+++ src/common/components/LoadingSpinner.tsx
@@ -4,7 +4,7 @@ import { PulseLoader } from "react-spinners";
 const LoadingSpinner = () => {
   return (
     <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-      <PulseLoader color="#36D7B7" size={25} />
+      <PulseLoader color="#1ED760" size={25} />
     </div>
   );
 };
